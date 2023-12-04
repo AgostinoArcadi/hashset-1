9cbd32f5346758d0377fbee28ed59f2011f61de6
Agostino <agostinoarcadi@gmail.com>
2023-12-04 11:41:04 +0100

    Esercizio finito

---
+++ README.md
@@ -0,0 +1,4 @@
+# hashset-1
+-Scrivere una funzione che restituisca un hashset riempito
+
+-Leggere l'hashSet e stampre grandezza ed elementi
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao");
+
+        for(Object word : setWords()){
+            System.out.println(word);
+        }
+
+        System.out.println(setWords().size());
+    }
+
+    public static HashSet setWords() {
+        HashSet<String> words = new HashSet<>();
+        words.add("ciao");
+        words.add("come");
+        words.add("stai");
+
+        return words;
     }
 }
